5da226b94e1c895b0922090986ac78175fade519
Sergey Malyuga <sergey@malyuga.ru>
2026-02-28 08:44:41 +0300

    feat(main.page): создать компонент

---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {HeaderComponent} from '../../features/header/header.component';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MainComponent {
+
+}
