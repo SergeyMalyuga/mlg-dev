f6adc4ebe791ff349f299b065e9daa66a427d89b
Sergey Malyuga <sergey@malyuga.ru>
2026-03-09 12:04:39 +0300

    feat(skills.component): создать пустой компонент
    - подключить к main.component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,13 +2,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {HeaderComponent} from '../../features/header/header.component';
 import {HeroComponent} from '../../features/hero/hero.component';
 import {AboutComponent} from '../../features/about/about.component';
+import {SkillsComponent} from '../../features/skills/skills.component';
 
 @Component({
   selector: 'app-main-page',
   imports: [
     HeaderComponent,
     HeroComponent,
-    AboutComponent
+    AboutComponent,
+    SkillsComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
